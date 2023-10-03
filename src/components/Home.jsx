faba4691d9d967ec6f2930680dc5f408d931f035
Muhammadqodir <muhammadqodirummatov31@gmail.com>
2023-10-03 15:27:52 +0200

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,18 +14,18 @@ const Home = () => {
     <div className='' style={{ width: '100%', height: '100vh' }}>
          <video className=' md:h-[100%] h-[100%]' style={{ objectFit: 'cover', width: '100%', }} src={bgv} autoPlay muted loop />
     </div>
-     <div className='absolute md:ml-[1300px]  ml-[320px] mt-[-540px] md:mt-[-500px] text-white text-2xl'>
+     <div className='absolute md:ml-[1300px]  ml-[270px] mt-[-540px] md:mt-[-500px] text-white text-2xl'>
      <img  className='rounded-full hover:scale-90 duration-500 w-[120px] md:w-[150px]' src={logo} />
      
      </div>
 
-     <div className='absolute px-40 md:px-32 text-black md:ml-[-400px] mt-[245px] md:mt-[270px] py-[10px] bg-slate-200 ml-[10px] md:mb-[-100px]  hover:scale-90 duration-500 shadow-md shadow-teal-300  rounded-md  group border-2 flex items-center hover:bg-violet-500  hover:border-violet-500'>
+     <div className='absolute px-40 md:px-32 text-black md:ml-[-400px] mt-[245px] md:mt-[270px] py-[10px] bg-slate-200 ml-[px] md:mb-[-100px]  hover:scale-90 duration-500 shadow-md shadow-teal-300  rounded-md  group border-2 flex items-center hover:bg-violet-500  hover:border-violet-500'>
            <button>
              <Link  spy={true} smooth={true} duration={500}  to='vehicle'> Buy Now </Link>
            </button>
       </div>
        
-       <div className='absolute px-28 md:px-20 text-white md:ml-[400px] mt-[360px] md:mt-[270px] py-[10px] bg-stone-800 ml-[10px] md:mb-[-100px]  hover:scale-90 duration-500 shadow-md shadow-teal-300  rounded-md  group border-2    flex items-center hover:bg-violet-500  hover:border-violet-500'>
+       <div className='absolute px-28 md:px-20 text-white md:ml-[400px] mt-[360px] md:mt-[270px] py-[10px] bg-stone-800 ml-[px] md:mb-[-100px]  hover:scale-90 duration-500 shadow-md shadow-teal-300  rounded-md  group border-2    flex items-center hover:bg-violet-500  hover:border-violet-500'>
            <button>
               <Link  spy={true} smooth={true} duration={500}  to='vehicle'> Discover Our Company </Link>
           </button>
